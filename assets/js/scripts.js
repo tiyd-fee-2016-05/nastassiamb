3c5416ab1b85dbe3a42fa26bc4405158aa319d39
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-06-08 18:34:21 -0400

    add js code trying to get repos to load

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -28,8 +28,10 @@ $(function () {
 
     var ghLogin = $('input[name="gh-login"]').val();
     $.getJSON('https://api.github.com/users/' + ghLogin)
-      .done(showUser)
+      .done(showUser,getSecondJason)
       .fail(showError);
+
+
   });
 
   function showUser(user) {
@@ -49,17 +51,47 @@ $(function () {
   }
 
 });
-// .............. my current experimental code below, from https://github.com/tiyd-fee-2016-05/lessons/blob/master/week-4/lodash.md
+
+var ghLogin = $('input[name="gh-login"]').val();
+function getSecondJason(){
+$.getJSON('https://api.github.com/users/' + ghLogin + "/repos")
+.done(showRepos)
+// need to replace showUser with a new template created in html
+;
+};
+
+
+function showRepos(repo) {
+  show2('gh-repo-template', repo);
+}
+
+function show2(template, model) {
+  var fn = _.template($('#' + template).html(), { variable: 'm' });
+  $('.gh-repo-template').html(fn(model));
+};
+
+
+//   show2('gh-repo-template', user);
+// }
+//
+//   function show2(template, model) {
+
+
+// .............. some experimental code below, from https://github.com/tiyd-fee-2016-05/lessons/blob/master/week-4/lodash.md
 
 
 // Get the template from the hacky script tag:
-var templateString = $('#myTemplate').html();
+// var templateString = $('#myTemplate').html();
 
 // Compile the template as per usual:
-var compiledTemplate = _.template(templateString, { variable: 'm' });
+// var compiledTemplate = _.template(templateString, { variable: 'm' });
 
 // Render the template out to our main div tag:
-$('#main').html(compiledTemplate({ name: 'John Smith' }));
+// $('.repoDivExperimental').html(compiledTemplate({ repos_url: 'John Smith' }));
+
+
+
+
 // .........................a middle-aged attempt below
 // <% m.repos_url.forEach(function (repomagic) { %>
 //     <div class="repomagic">
